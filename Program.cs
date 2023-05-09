7316abcd1daf8a0a16fd0036d79cb0dc98e0b485
VictorJust <victorjust09@gmail.com>
2023-05-09 13:42:57 +0300

    Add Computer Club manager program

---
+++ Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComputerClub
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ComputerClub computerClub = new ComputerClub(8);
+            computerClub.Work();
+        }
+    }
+
+    class ComputerClub
+    {
+        private int _money = 0;
+        private List<Computer> _computers = new List<Computer>();
+        private Queue<Client> _clients = new Queue<Client>();
+
+        public ComputerClub(int computersCount)
+        {
+            Random random = new Random();
+    
+            for (int i = 0; i < computersCount; i++)
+            {
+                _computers.Add(new Computer(random.Next(5, 15)));
+            }
+            
+            CreateNewClients(25, random);
+        }
+
+        public void CreateNewClients(int count, Random moneyOwned)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _clients.Enqueue(new Client(moneyOwned.Next(100, 251), moneyOwned));
+            }
+        }
+
+        public void Work()
+        {
+            while (_clients.Count > 0)
+            {
+                Client newClient = _clients.Dequeue();
+                Console.WriteLine($"Computer club's balance: ${_money}. Waiting for a new client.");
+                Console.WriteLine($"We have a new client. He wants to rent a computer for {newClient.DesiredMinutes} minutes.");
+                ShowAllComputersState();
+
+                Console.Write("\nYou offer him computer number: ");
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int computerNumber))
+                {
+                    computerNumber -= 1;
+
+                    if (computerNumber >= 0 && computerNumber < _computers.Count)
+                    {
+                        if (_computers[computerNumber].IsTaken)
+                        {
+                            Console.WriteLine("You're trying to choose the computer which is already taken.");
+                        }
+                        else
+                        {
+                            if (newClient.CheckSolvency(_computers[computerNumber]))
+                            {
+                                Console.WriteLine($"The client has paid and has sat at computer number {computerNumber + 1}.");
+                                _money += newClient.Pay();
+                                _computers[computerNumber].BecomeTaken(newClient);
+                            }
+                            else
+                            {
+                                Console.WriteLine("The client doesn't have enough money. The client leaves.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You don't know which computer to choose. The client gets angry and goes away.");
+                    }
+                }
+                else
+                {
+                    CreateNewClients(1, new Random());
+                    Console.WriteLine("Wrong input. Try again.");
+                }
+
+                Console.WriteLine("Press any key to switch to the next client.");
+                Console.ReadKey();
+                Console.Clear();
+                SpendOneMinute();
+            }
+        }
+
+        private void ShowAllComputersState()
+        {
+            Console.WriteLine("\nAll computers list:");
+            for (int i = 0; i < _computers.Count; i++)
+            {
+                Console.Write(i + 1 + " - ");
+                _computers[i].ShowState();
+            }
+        }
+
+        private void SpendOneMinute()
+        {
+            foreach (var computer in _computers)
+            {
+                computer.SpendOneMinute();
+            }
+        }
+    }
+
+    class Computer
+    {
+        private Client _client;
+        private int _minutesRemaining;
+
+        public bool IsTaken
+        {
+            get { return _minutesRemaining > 0; }
+        }
+
+        public int PricePerMinute { get; private set; }
+
+        public Computer(int pricePerMinute)
+        {
+            PricePerMinute = pricePerMinute;
+        }
+
+        public void BecomeTaken(Client client)
+        {
+            _client = client;
+            _minutesRemaining = _client.DesiredMinutes;
+        }
+
+        public void BecomeEmpty()
+        {
+            _client = null;
+        }
+
+        public void SpendOneMinute()
+        {
+            _minutesRemaining--;
+        }
+
+        public void ShowState()
+        {
+            if(IsTaken)
+                Console.WriteLine($"The computer is taken. Minutes remaining: {_minutesRemaining}.");
+            else
+                Console.WriteLine($"The computer is available. Price per minute: {PricePerMinute}.");
+        }
+    }
+
+    class Client
+    {
+        private int _money;
+        private int _moneyToPay;
+        
+        public int DesiredMinutes { get; private set; }
+
+        public Client(int money, Random random)
+        {
+            _money = money;
+            DesiredMinutes = random.Next(10, 30);
+        }
+
+        public bool CheckSolvency(Computer computer)
+        {
+            _moneyToPay = DesiredMinutes * computer.PricePerMinute;
+
+            if (_money >= _moneyToPay)
+            {
+                return true;
+            }
+            else
+            {
+                _moneyToPay = 0;
+                return false;
+            }
+        }
+
+        public int Pay()
+        {
+            _money -= _moneyToPay;
+            return _moneyToPay;
+        }
+    }
+}
